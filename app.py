8f5f24297031fc32fa9799fd69d82e58d2b7f845
Joseph Mtinangi <josephmtinangi@gmail.com>
2017-06-10 13:45:44 +0300

    There was power disrupt

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return '<h1>Hello Flask 101</h1>';
+    return render_template('index.html')
+
+
+@app.route('/about')
+def about():
+    return 'about'
+
+
+@app.route('/contact')
+def contact():
+    return 'contact'
 
 
 if __name__ == '__main__':
